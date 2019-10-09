fd3e09042599e40580ca2525d4179ed366c9d9c1
Tomoyuki Oota <o9o9@o9o9Mac.local>
2019-10-09 22:08:16 +0900

    commit

--- SQL_Server_Autotune/0.restorewwi_linux.sql
+++ SQL_Server_Autotune/0.restorewwi_linux.sql
@@ -1,4 +1,4 @@
-restore database WideWorldImporters from disk = '/var/opt/mssql/WideWorldImporters-Full.bak' with
+restore database demodb from disk = '/tmp/WideWorldImporters-Full.bak' with
 move 'WWI_Primary' to '/var/opt/mssql/data/WideWorldImporters.mdf',
 move 'WWI_UserData' to '/var/opt/mssql/data/WideWorldImporters_UserData.ndf',
 move 'WWI_Log' to '/var/opt/mssql/data/WideWorldImporters.ldf',
