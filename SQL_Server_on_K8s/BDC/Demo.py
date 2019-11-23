fbd5274172349af2b794585dc0dcf7da3fd9e751
Tomoyuki Oota <o9o9@mac19.local>
2019-11-23 10:44:44 +0900

    commit

--- SQL_Server_on_K8s/BDC/Demo.py
+++ SQL_Server_on_K8s/BDC/Demo.py
@@ -11,7 +11,8 @@
 # ![](image/2019-09-29-01-47-54.png)
 
 #%% [markdown]
-# # 1. 事前に準備済みのK8s環境を確認
+# # 1. BDC環境確認
+
 #%% [markdown]
 # ## K8s
 #%% [markdown]
@@ -34,6 +35,45 @@ az vm list -g MC_RG_BDC_O9O9AKS_JAPANEAST -d
 kubectl describe node
 
 #%% [markdown]
+# ## Pod
+#%% [markdown]
+%%bash
+kubectl get pods -n mssql-cluster
+# podの詳細
+# kubectl describe pod <pod_name> -n mssql-cluster
+# podのログ取得
+# kubectl logs <pod_name> --all-containers=true -n mssql-cluser > containers-log.txt
+# kubectl logs <pod_name> -c <container_name> -n mssql-cluser > container-log.txt
+
+#%% [markdown]
+# ## Service
+#%% [markdown]
+%%bash
+kubectl get svc -n mssql-cluster
+# サービス詳細
+# kubectl describe service <service_name> -n mssql-cluster
+
+#%% [markdown]
+# ## Container
+# kubectl exec -it <pod_name>  -c <container_name> -n <namespace_name> -- /bin/bash <command name> 
+
+#%% [markdown]
+# ## BDC
+#%% [markdown]
+%%bash
+azdata bdc control status show -o table
+#%% [markdown]
+%%bash
+azdata bdc status show -o table
+
+
+#%% [markdown]
+kubectl get node -o wide
+az vm list -g MC_RG_BDC_O9O9AKS_JAPANEAST -d
+az aks show -g rg_bdc -n o9o9aks
+az aks browse -g rg_bdc --listen-address 0.0.0.0 -n o9o9aks
+
+#%% [markdown]
 # # 2. operator作成
 %%bash
 cat ~/Jupyter/SQL_Server_on_K8s/AG/1.operator.yaml
@@ -50,97 +90,40 @@ cat ~/Jupyter/SQL_Server_on_K8s/AG/3.ag-services.yaml
 
 #%% [markdown]
 # # 5. 環境確認
-#%% [markdown]
-%%bash
 kubectl get pod -o wide -n mssql-ag
-#%% [markdown]
-%%bash
 kubectl get pvc -o wide -n mssql-ag
 # kubectl describe pvc -n mssql-ag
-#%% [markdown]
-%%bash
 az disk list -g MC_RG_BDC_O9O9AKS_JAPANEAST
-#%% [markdown]
-%%bash
 kubectl get service -o wide -n mssql-ag
 
-#%% [markdown]
-# # 6. AG確認（DMV）
-#%% [markdown]
-%%bash
+# AG確認（DMV）
 sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'select r.replica_server_name, rs.is_primary_replica, rs.synchronization_health_desc FROM sys.dm_hadr_database_replica_states rs JOIN sys.availability_replicas r ON r.group_id = rs.group_id AND r.replica_id = rs.replica_id'
-#%% [markdown]
-%%bash
 sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'select @@servername'
-#%% [markdown]
-%%bash
 sqlcmd -S 20.43.76.137 -U sa -P $PASSWORD -Q 'select @@servername'
-
-#%% [markdown]
-# # 6. AG確認（SSMS）
-
-#%% [markdown]
-# # 6. AG確認（K8s Dashboard）
-#%% [markdown]
-%%bash
+# AG確認（SSMS）
+# AG確認（K8s Dashboard）
 az aks browse -g rg_bdc --listen-address 0.0.0.0 -n o9o9aks
 
-#%% [markdown]
-# # 7.セカンダリから接続（ReadOnlyでないとエラーになる）
-#%% [markdown]
-%%bash
+# セカンダリから接続（ReadOnlyでないとエラーになる）
 sqlcmd -S 20.43.76.137 -U sa -P $PASSWORD -Q 'use agdb; select * from tab01'
-#%% [markdown]
-%%bash
 sqlcmd -S 20.43.76.137 -U sa -P $PASSWORD -Q 'use agdb; select * from tab01' -K READONLY
 
-#%% [markdown]
-# # 8. AGの機能でフェールオーバー（エラーになる）
-#%% [markdown]
-%%bash
+# AGの機能でフェールオーバー（エラーになる）
 sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'ALTER AVAILABILITY GROUP agdb FAILOVER'
 
-#%% [markdown]
-# # 9. K8sのJobでフェールオーバー
-#%% [markdown]
-%%bash
+# K8sのJobでフェールオーバー
 # kubectl apply -f ~/Jupyter/SQL_Server_on_K8s/AG/4-1.failover_to_mssql1-0.yaml -n mssql-ag
 kubectl apply -f ~/Jupyter/SQL_Server_on_K8s/AG/4-2.failover_to_mssql2-0.yaml -n mssql-ag
 # kubectl apply -f ~/Jupyter/SQL_Server_on_K8s/AG/4-3.failover_to_mssql3-0.yaml -n mssql-ag
-#%% [markdown]
-%%bash
 kubectl get jobs -n mssql-ag
 
-#%% [markdown]
-# # 10. AG確認
-#%% [markdown]
-%%bash
+# AG確認
 sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'select r.replica_server_name, rs.is_primary_replica, rs.synchronization_health_desc FROM sys.dm_hadr_database_replica_states rs JOIN sys.availability_replicas r ON r.group_id = rs.group_id AND r.replica_id = rs.replica_id'
-#%% [markdown]
-%%bash
 sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'select @@servername'
-#%% [markdown]
-%%bash
 sqlcmd -S 20.43.76.137 -U sa -P $PASSWORD -Q 'select @@servername'
 
-#%% [markdown]
-# # （11. F/Oジョブの削除）
-#%% [markdown]
-%%bash
 # kubectl delete -f ~/Jupyter/SQL_Server_on_K8s/AG/4-1.failover_to_mssql1-0.yaml -n mssql-ag
 kubectl delete -f ~/Jupyter/SQL_Server_on_K8s/AG/4-2.failover_to_mssql2-0.yaml -n mssql-ag
 # kubectl delete -f ~/Jupyter/SQL_Server_on_K8s/AG/4-3.failover_to_mssql3-0.yaml -n mssql-ag
-#%% [markdown]
-%%bash
 kubectl get jobs -n mssql-ag
 
-
-#%% [markdown]
-# # （12. Primaryをmssql1−0に戻しておく）
-#%% [markdown]
-%%bash
-kubectl apply -f ~/Jupyter/SQL_Server_on_K8s/AG/4-1.failover_to_mssql1-0.yaml -n mssql-ag
-kubectl get jobs -n mssql-ag
-sqlcmd -S 40.115.179.101 -U sa -P $PASSWORD -Q 'select r.replica_server_name, rs.is_primary_replica, rs.synchronization_health_desc FROM sys.dm_hadr_database_replica_states rs JOIN sys.availability_replicas r ON r.group_id = rs.group_id AND r.replica_id = rs.replica_id'
-kubectl delete -f ~/Jupyter/SQL_Server_on_K8s/AG/4-1.failover_to_mssql1-0.yaml -n mssql-ag
-kubectl get jobs -n mssql-ag
--- SQL_Server_on_K8s/FCI/Demo.py
+++ SQL_Server_on_K8s/FCI/Demo.py
@@ -10,56 +10,111 @@
 # # Image
 # ![](image/2019-09-29-01-47-54.png)
 
-
 #%% [markdown]
 # # 1. 事前に準備済みのK8s環境を確認
+#%% [markdown]
+# ## K8s
+#%% [markdown]
 %%bash
-kubectl get node -o wide
-az vm list -g MC_RG_BDC_O9O9AKS_JAPANEAST -d
 az aks show -g rg_bdc -n o9o9aks
+#%% [markdown]
+%%bash
 az aks browse -g rg_bdc --listen-address 0.0.0.0 -n o9o9aks
 
 #%% [markdown]
-# # 2. PV作成
-cd ~/Jupyter/SQL_Server_on_K8s/FCI
-cat pvc.yaml
-kubectl apply -f pvc.yaml -n mssql-fci
+# ## Node
+#%% [markdown]
+%%bash
+kubectl get node -o wide
+#%% [markdown]
+%%bash
+az vm list -g MC_RG_BDC_O9O9AKS_JAPANEAST -d
+#%% [markdown]
+%%bash
+kubectl describe node
 
-# # 3. PV確認 
+#%% [markdown]
+# # 2. PV確認
+#%% [markdown]
+%%bash
+cat ~/Jupyter/SQL_Server_on_K8s/FCI/pvc.yaml
+
+#%% [markdown]
+%%bash
 kubectl get sc -o wide
 kubectl describe sc azure-disk
+
+#%% [markdown]
+%%bash
 kubectl get pv -o wide -n mssql-fci
 kubectl describe pv -n mssql-fci
+
+#%% [markdown]
+%%bash
 kubectl get pvc -o wide -n mssql-fci
 kubectl describe pvc -n mssql-fci
+
+#%% [markdown]
+%%bash
 az disk list -g MC_RG_BDC_O9O9AKS_JAPANEAST 
 
-# # 4. SQL（Deployment、Service）作成
-cat sql19deployment.yaml
-kubectl apply -f sql19deployment.yaml -n mssql-fci
+#%% [markdown]
+# # 3. Pod確認
+#%% [markdown]
+%%bash
+cat ~/Jupyter/SQL_Server_on_K8s/FCI/sql19deployment.yaml
 
-# # 5. SQL確認
+#%% [markdown]
+%%bash
 kubectl get pod -o wide -n mssql-fci
 kubectl describe pod -n mssql-fci
+
+#%% [markdown]
+%%bash
 kubectl get service -o wide -n mssql-fci
 kubectl describe service -n mssql-fci
 
-# # 6. SQL接続テスト
-kubectl exec mssql-deployment-79fdff6b95-ldwps -c mssql -n mssql-fci -- /opt/mssql-tools/bin/sqlcmd -U sa -P $PASSWORD -Q 'SELECT @@VERSION'
+#%% [markdown]
+# # 4. SQL接続テスト
+#%% [markdown]
+%%bash
+kubectl exec mssql-deployment-79fdff6b95-nk8zx -c mssql -n mssql-fci -- /opt/mssql-tools/bin/sqlcmd -U sa -P $PASSWORD -Q 'SELECT @@VERSION'
+#%% [markdown]
+%%bash
 sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'select @@version'
+#%% [markdown]
+%%bash
 sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'create database FCIDB'
 sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'use FCIDB; create table tab01(id int, name varchar(max)); insert into tab01 values (1, "SQL Server runs on K8s!")'
-sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'select name from sys.databases; use FCIDB; select * from tab01'
+sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'use FCIDB; select * from tab01'
+
+#%% [markdown]
 #kubectl exec mssql-deployment-79fdff6b95-ldwps -c mssql -n mssql-fci -- /opt/mssql-tools/bin/sqlcmd -U sa -P $PASSWORD -Q 'create database FCIDB'
 #kubectl exec mssql-deployment-79fdff6b95-ldwps -c mssql -n mssql-fci -- /opt/mssql-tools/bin/sqlcmd -U sa -P $PASSWORD -Q 'use FCIDB; create table tab01(id int, name varchar(max)); insert into tab01 values (1, "SQL Server runs on K8s!")'
 #kubectl exec mssql-deployment-79fdff6b95-ldwps -c mssql -n mssql-fci -- /opt/mssql-tools/bin/sqlcmd -U sa -P $PASSWORD -Q 'select name from sys.databases; use FCIDB; select * from tab01'
-
 #sqlcmd -S localhost,11433 -U sa -P $PASSWORD -Q 'create database FCIDB'
 #sqlcmd -S localhost,11433 -U sa -P $PASSWORD -Q 'use FCIDB; create table tab01(id int, name varchar(max)); insert into tab01 values (1, "SQL Server runs on K8s!")'
 #sqlcmd -S localhost,11433 -U sa -P $PASSWORD -Q 'select name from sys.databases; use FCIDB; select * from tab01'
 
+#%% [markdown]
+# # 5. Pod障害擬似
+#%% [markdown]
+%%bash
+kubectl get pod -o wide -n mssql-fci
 
-# pod障害
+#%% [markdown]
+%%bash
+kubectl delete pod mssql-deployment-79fdff6b95-nk8zx
+
+#%% [markdown]
+%%bash
+kubectl get pod -o wide -n mssql-fci
+
+#%% [markdown]
+%%bash
+sqlcmd -S 52.185.174.22 -U sa -P $PASSWORD  -Q 'use FCIDB; select * from tab01'
+
+
 
 M-x shell
 C-x o
@@ -69,12 +124,3 @@ C-u M-x shell -> shell2
 C-x o
 C-x o
 C-x 0
-
-
-	○ session1（pod監視）
-	kubectl get pod -o wide -n mssql-fci -w
-
-	○ session2（pod障害疑似）
-	kubectl get pod -o wide -n mssql-fci
-	kubectl delete pod <pod>
-	sqlcmd -S 40.115.245.30 -U sa -P $SAPASSWORD  -Q 'select @@version'
